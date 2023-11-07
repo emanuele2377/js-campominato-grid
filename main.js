1d69ce456e977abb44966713c481950e05648920
emanuele2377 <emanuelekushi7@gmail.com>
2023-11-07 17:11:59 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+// <div class="square"></div>
+
+//creare la griglia dinamicamente, inserendo N elementi .square nel container
+let numCelle = 100;
+let grid = document.getElementById("grid");
+
+ 
+for ( let i = 1; i <= 100; i++) {
+  
+}
+
+for (let i = 1; i <= numCelle; i++) {
+    
+    let cella = creaQuadrato(i);
+    grid.appendChild(cella);
+
+}
+
+/******************************************************************** */
+
+function creaQuadrato(numero) {
+
+    const cella = document.createElement("div");
+    cella.classList.add("square");
+
+    //per ogni quadrato voglio un evento che gestisca il click
+    cella.addEventListener("click", function() {
+
+        console.log("Cella cliccata", numero);
+        
+        //versione abbreviata
+        cella.classList.toggle("highlight");
+
+        
+
+    });
+
+    return cella;
+}
